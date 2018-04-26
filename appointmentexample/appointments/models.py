2cf1f6bb05c606dcfb5f15117e00b93a424ef072
ikanreed <adrian.cline@gmail.com>
2018-04-25 20:04:16 -0400

    hey, the admin kinda sucks with default __str__

--- appointmentexample/appointments/models.py
+++ appointmentexample/appointments/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Appointment(models.Model):
     time=models.DateTimeField()
     description=models.CharField(max_length=640)
+    #let's make the admin kinda better to use
+    def __str__(self):
+        return "%s at %s"%(self.description, self.time)
